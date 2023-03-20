851a6ee9f59f546d165a411a04569867f4c95efa
LumaGhost <29874438+LumaGhost@users.noreply.github.com>
2023-03-20 16:51:20 +0000

    remove hard coded target dir

--- rust_in_cpp/cpp_bindings_rust_lib/cargo_build.py
+++ rust_in_cpp/cpp_bindings_rust_lib/cargo_build.py
@@ -9,13 +9,14 @@ parser = argparse.ArgumentParser(
                     description='What the program does',
                     epilog='Text at the bottom of help')
 parser.add_argument('--cargo_manifest_path')
+parser.add_argument('--cargo_target_dir')
 
 def main():
-    cargo_target_dir = "/workspaces/cpp20_rust/rust_in_cpp/rust_lib/rust_target"
     args = parser.parse_args()
-    subprocess.run(['cargo', 'build', '-vv', '--release', '--manifest-path', args.cargo_manifest_path], check=True)
-    shutil.copyfile(os.path.join(cargo_target_dir,"release","librust_src.so"), os.path.join(os.getcwd(), "librust_src.so"))
-    shutil.copyfile(os.path.join(cargo_target_dir,"rust_src.hpp"), os.path.join(os.getcwd(), "rust_src.hpp"))
+    os.environ['CARGO_TARGET_DIR'] = args.cargo_target_dir
+    subprocess.run(['cargo', 'build', '-vv', '--release', '--manifest-path', args.cargo_manifest_path], check=True, env=os.environ)
+    shutil.copyfile(os.path.join(args.cargo_target_dir,"release","librust_src.so"), os.path.join(os.getcwd(), "librust_src.so"))
+    shutil.copyfile(os.path.join(args.cargo_target_dir,"rust_src.hpp"), os.path.join(os.getcwd(), "rust_src.hpp"))
 
 if __name__ == "__main__":
     main()
