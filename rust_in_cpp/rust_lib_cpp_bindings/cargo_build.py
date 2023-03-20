270ac7b9e11e5010bc1ab2ce9b6c4a0ed395ba0c
LumaGhost <29874438+LumaGhost@users.noreply.github.com>
2023-03-20 18:25:42 +0000

    update build scripts for new naming conventions

--- rust_in_cpp/rust_lib_cpp_bindings/cargo_build.py
+++ rust_in_cpp/rust_lib_cpp_bindings/cargo_build.py
@@ -15,8 +15,8 @@ def main():
     args = parser.parse_args()
     os.environ['CARGO_TARGET_DIR'] = args.cargo_target_dir
     subprocess.run(['cargo', 'build', '-vv', '--release', '--manifest-path', args.cargo_manifest_path], check=True, env=os.environ)
-    shutil.copyfile(os.path.join(args.cargo_target_dir,"release","librust_src.so"), os.path.join(os.getcwd(), "librust_src.so"))
-    shutil.copyfile(os.path.join(args.cargo_target_dir,"rust_src.hpp"), os.path.join(os.getcwd(), "rust_src.hpp"))
+    shutil.copyfile(os.path.join(args.cargo_target_dir,"release","librust_lib_c_abi.so"), os.path.join(os.getcwd(), "librust_lib_c_abi.so"))
+    shutil.copyfile(os.path.join(args.cargo_target_dir,"rust_lib_c_abi.hpp"), os.path.join(os.getcwd(), "rust_lib_c_abi.hpp"))
 
 if __name__ == "__main__":
     main()
