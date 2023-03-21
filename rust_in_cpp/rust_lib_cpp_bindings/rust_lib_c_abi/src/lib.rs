277394394de51a103d00429d605dccecc47faca6
LumaGhost <29874438+LumaGhost@users.noreply.github.com>
2023-03-21 15:42:07 +0000

    add a dependency on the rust side

--- rust_in_cpp/rust_lib_cpp_bindings/rust_lib_c_abi/src/lib.rs
+++ rust_in_cpp/rust_lib_cpp_bindings/rust_lib_c_abi/src/lib.rs
@@ -1,6 +1,11 @@
 
 
+use chrono::prelude::*;
+
 #[no_mangle]
 pub extern "C" fn meep() {
     println!("hello from rust uwu");
+    let utc: DateTime<Utc> = Utc::now();
+    let local: DateTime<Local> = Local::now();
+    println!("utc time: {}, local time: {}", utc, local);
 }
