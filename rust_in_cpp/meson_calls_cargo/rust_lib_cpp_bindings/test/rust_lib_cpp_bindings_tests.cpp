4a85e4711532158036ed82d3d1d515fcaf85fe36
LumaGhost <29874438+LumaGhost@users.noreply.github.com>
2023-03-22 20:50:09 +0000

    add some cpp20 code to the rust lib cpp bindings tests

--- rust_in_cpp/meson_calls_cargo/rust_lib_cpp_bindings/test/rust_lib_cpp_bindings_tests.cpp
+++ rust_in_cpp/meson_calls_cargo/rust_lib_cpp_bindings/test/rust_lib_cpp_bindings_tests.cpp
@@ -1,8 +1,11 @@
 
-
+#include <iostream>
+#include <numbers>
 #include <rust_lib_cpp_bindings.hpp>
 
 
 int main() {
     ffi::meep();
+    std::cout << "hello (:" << std::endl;
+    std::cout << std::numbers::pi << std::endl;
 }
